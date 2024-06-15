413d794de598c000de6ccb3a3073c10c6ef5c9fa
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-06-15 08:41:33 +0300

    initial commit

--- spring-rabbitmq/src/main/java/com/example/rabbitmq_api/repositorys/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rabbitmq_api.repositorys;
-
-import com.example.rabbitmq_api.models.MessageEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends CrudRepository<MessageEntity, Long> {
-}
